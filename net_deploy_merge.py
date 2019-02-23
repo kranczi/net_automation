334594f32a2db67a5e56b50fd3229bc967db3dc9
Marcin Adamski <adamski.marcin@gmail.com>
2019-02-23 17:05:13 +0100

    add function for node select (static method);  add NetView class

--- net_deploy_merge.py
+++ net_deploy_merge.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python2.7
 import sys
 import time
+import json
 from napalm import get_network_driver
 
 
-class Net_Node:
+dcs_raw = 'dc.json'
+dc_sel = str(raw_input('select dc (or type \"all\" is for all): '))
+role_sel = str(raw_input('select role: '))
+with open(dcs_raw, 'r') as dcs_file:
+    dcs_db = json.load(dcs_file)
+
+class NetNode:
 
     def __init__(self, hostname, username='user', password='pass'):
         self.hostname = hostname
@@ -16,10 +23,10 @@ class Net_Node:
         self.node_driver = driver(self.hostname, self.username, self.password)
 
     """
-	Default netconf rpc timeout is 30 seconds.
-	This is not enough for srx cluster to commit the config.
-	Increase it to avoid RpcTimeoutError or TimeoutExpiredError
-	"""
+    Default netconf rpc timeout is 30 seconds.
+    This is not enough for srx cluster to commit the config.
+    Increase it to avoid RpcTimeoutError or TimeoutExpiredError
+    """
 
     def node_rpc_timeout(self):
         self.node_driver.timeout = 300
@@ -47,6 +54,10 @@ class Net_Node:
     def node_discard(self):
         self.node_driver.discard()
 
+    def node_view_users(self):
+        users = self.node_driver.get_users()
+        print(users)
+
     def node_decision(self):
         change_decision = raw_input('commit or discard?: ')
         if change_decision == 'commit':
@@ -60,16 +71,31 @@ class Net_Node:
             print('something other')
             self.node_discard()
             raise sys.exit()
+    
+    @staticmethod
+    def node_select(dc_sel, role_sel, dcs_db):
+        for k, v in dcs_db.items():
+            for k1, v1 in v.items():
+                if isinstance(v1, dict):
+                    if dc_sel == 'all':
+                        NetNode.node_select(dc_sel, role_sel, v1)
+                        for k2, v2 in v1.items():
+                            if k2.startswith(role_sel):
+                                nodes_selected = v2.values()
+                                return nodes_selected
+                    if k1 == dc_sel:
+                        NetNode.node_select(dc_sel, role_sel, v1)
+                        for k2, v2 in v1.items():
+                            if k2.startswith(role_sel):
+                                nodes_selected = v2.values()
+                                return nodes_selected
 
-optimus_prime = ['node1', 'node2']
-megatron = ['node3', 'node4']
 
-
-class Net_Commit:
+class NetCommit:
 
     def __init__(self):
-        for i in self.group_decision():
-            self = Net_Node(i)
+        for i in NetNode.node_select(dc_sel, role_sel, dcs_db):
+            self = NetNode(i)
             self.node_driver()
             self.node_open()
             self.node_merge()
@@ -79,18 +105,24 @@ class Net_Commit:
             time.sleep(30)
             self.node_close()
 
-    def group_decision(self):
-        self = []
-        print('1: ' + str(optimus_prime))
-        print('2: ' + str(megatron))
-        group_input = raw_input('choose from 1 or 2: ')
-        if group_input == '1':
-            self = optimus_prime
-        elif group_input == '2':
-            self = megatron
-        return self
+
+class NetView:
+
+    def __init__(self):
+        for i in NetNode.node_select(dc_sel, role_sel, dcs_db):
+            self = NetNode(i)
+            self.node_driver()
+            self.node_rpc_timeout()
+            self.node_open()
+            self.node_view_users()
+            self.node_close()
+
 
 if __name__ == "__main__":
-    Net_Commit()
+    NetView()
+#    NetNode()
+#    NetCommit()
+
+
 
 sys.exit()
